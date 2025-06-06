11dd054ace3d080bf91c5706b89ddabc02433827
Diego17 <diegoalbertocastropastor@gmail.com>
2025-06-05 20:19:34 -0500

    Update main class path to reflect new package structure

---
+++ MoviesRecommender/src/com/dialca/recommender/app/MainApp.java
@@ -0,0 +1,23 @@
+package com.dialca.recommender.app;
+// Author: DialcaDev / Diego Castro
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hola mundo");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 200); 
+        
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
