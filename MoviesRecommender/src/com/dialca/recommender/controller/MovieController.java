56e7d8c027d47e050ec749b04d024d21b2bf9c04
Diego17 <diegoalbertocastropastor@gmail.com>
2025-06-05 19:56:41 -0500

    Add MovieController with methods to manage movies

---
+++ MoviesRecommender/src/com/dialca/recommender/controller/MovieController.java
@@ -0,0 +1,32 @@
+package com.dialca.recommender.controller;
+
+import com.dialca.recommender.dao.MovieDao;
+import com.dialca.recommender.model.Movie;
+
+import java.util.List;
+
+public class MovieController {
+    private final MovieDao movieDao = new MovieDao();
+
+    public boolean addMovie(String title, String description, String genre, int year, String poster_url) {
+        Movie movie = new Movie(title, description, genre, year, poster_url);
+        return movieDao.create(movie);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieDao.getAll();
+    }
+
+    public Movie getMovieById(int id) {
+        return movieDao.findById(id);
+    }
+    public Movie getMovieByTitle(String title) {
+        return movieDao.findByTitle(title);
+    }
+    public List<Movie> getMoviesByGenre(String genre) {
+        return movieDao.findByGenre(genre);
+    }
+    public List<Movie> searchMovies(String query){
+        return movieDao.searchMovies(query);
+    }
+}
